61d7eda57a29a469e58c158e3a11866b5a99b2a6
TAWEESOFT <TAWEESOFT@TAWEESOFTs-MacBook-Pro.local>
2016-10-08 17:32:30 +0700

    index page support responsive

--- app/assets/javascripts/components/index/sections/index/desktop/wrapper.jsx
+++ app/assets/javascripts/components/index/sections/index/desktop/wrapper.jsx
@@ -3,29 +3,29 @@ class Desktop extends React.Component {
 		return (
 			<div className="uk-width-1-1 event-info uk-flex uk-flex-center">
 				<div className="uk-width-1-1 uk-grid uk-flex uk-flex-center uk-margin-bottom">
-					<div className="uk-width-medium-3-10 uk-width-large-3-10 uk-width-small-1-1 uk-flex uk-flex-center uk-flex-middle">
+					<div className="uk-width-medium-3-10 uk-width-large-3-10 uk-width-small-1-1 uk-flex uk-flex-center uk-flex-middle uk-margin-bottom">
 						<div className="uk-width-1-6 uk-flex uk-flex-center">
 							<img className="icon" src={this.props.schedule}/>
 						</div>
-						<div className="uk-margin-left uk-width-3-6 uk-width-medium-5-6 uk-width-large-3-6">
+						<div className="index-info-text uk-margin-left uk-width-3-10 uk-width-medium-5-6 uk-width-large-4-6">
 							<h3 className="no-margin">13 November 2016</h3>
 							<h3 className="no-margin">9.00 AM - 5.00 PM</h3>
 						</div>
 					</div>
-					<div className="uk-width-medium-3-10 uk-width-large-3-10 uk-width-small-1-1 uk-flex uk-flex-center uk-flex-middle">
+					<div className="uk-width-medium-3-10 uk-width-large-3-10 uk-width-small-1-1 uk-flex uk-flex-center uk-flex-middle uk-margin-bottom">
 						<div className="uk-width-1-6 uk-flex uk-flex-center">
 							<img className="icon" src={this.props.location}/>
 						</div>
-						<div className="uk-margin-left uk-width-3-6 uk-width-medium-5-6 uk-width-large-3-6">
+						<div className="index-info-text uk-margin-left uk-width-3-10 uk-width-medium-5-6 uk-width-large-4-6">
 							<h3 className="no-margin">IUP Building (17)</h3>
 							<h3 className="no-margin">Kasetsart University</h3>
 						</div>
 					</div>
-					<div className="uk-width-medium-3-10 uk-width-large-3-10 uk-width-small-1-1 uk-flex uk-flex-center uk-flex-middle">
+					<div className="uk-width-medium-3-10 uk-width-large-3-10 uk-width-small-1-1 uk-flex uk-flex-center uk-flex-middle uk-margin-bottom">
 						<div className="uk-width-1-6 uk-flex uk-flex-center">
 							<img className="icon" src={this.props.twitter}/>
 						</div>
-						<div className="uk-margin-left uk-width-3-6 uk-width-medium-5-6 uk-width-large-3-6">
+						<div className="index-info-text uk-margin-left uk-width-3-10 uk-width-medium-5-6 uk-width-large-4-6">
 							<h1 className="no-margin">#bcbk</h1>
 						</div>
 					</div>
