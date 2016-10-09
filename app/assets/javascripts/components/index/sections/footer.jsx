25fbf30b21a8aa913486118da164ceaa24696592
TAWEESOFT <TAWEESOFT@TAWEESOFTs-MacBook-Pro.local>
2016-10-09 14:07:49 +0700

    add link to social media and add facebook on index page

--- app/assets/javascripts/components/index/sections/footer.jsx
+++ app/assets/javascripts/components/index/sections/footer.jsx
@@ -8,8 +8,12 @@ class Footer extends React.Component {
         <div className="uk-flex uk-flex-center uk-flex-column uk-width-medium-1-4 uk-width-large-1-4 uk-width-small-1-1 uk-margin-top uk-margin-bottom">
           <p className="uk-text-uppercase uk-text-center">contact with us</p>
           <div className="flex flex-center uk-margin-top">
-            <img className="social-icon" src={this.props.facebookLogo}/>
-            <img className="uk-margin-left social-icon" src={this.props.twitterLogo} />
+            <a href="https://www.facebook.com/Barcampbangkhen/" target="_blank">
+              <img className="social-icon"  src={this.props.facebookLogo}/>
+            </a>
+            <a href="https://twitter.com/barcampbangkhen" target="_blank">
+              <img className="uk-margin-left social-icon" src={this.props.twitterLogo} />
+            </a>
           </div>
         </div>
         <div id="contact-email" className="uk-text-right uk-width-medium-1-4 uk-width-large-1-4 uk-width-small-1-1 uk-margin-top uk-margin-bottom">
