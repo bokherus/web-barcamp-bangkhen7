00099409fbee312fa9afda4afa72fc531b9f2bc2
Thongrapee <kulaspa@gmail.com>
2016-10-11 21:49:59 +0700

    Try new signin mechanism

--- app/controllers/registrations_controller.rb
+++
@@ -1,4 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-    clear_respond_to
-    respond_to :json
-end
--- app/controllers/sessions_controller.rb
+++
@@ -1,4 +0,0 @@
-class SessionsController < Devise::SessionsController
-    clear_respond_to
-    respond_to :json
-end
