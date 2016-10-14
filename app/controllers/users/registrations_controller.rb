bcb6408ee238eb365a44991bfe53b68b4fe802c2
TAWEESOFT <TAWEESOFT@TAWEESOFTs-MacBook-Pro.local>
2016-10-14 16:11:49 +0700

    show latest participant and prevent signin after aign up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -28,7 +28,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     if resource.persisted?
       if resource.active_for_authentication?
         set_flash_message! :notice, :signed_up
-        sign_up(resource_name, resource)
+        # sign_up(resource_name, resource)
         respond_with resource, location: after_sign_up_path_for(resource)
       else
         set_flash_message! :notice, :signed_up_but_#{resource.inactive_message}"
