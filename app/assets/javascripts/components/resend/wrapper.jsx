468ee5f322e2603c2f626ffc937c239d3b46d894
TAWEESOFT <TAWEESOFT@TAWEESOFTs-MacBook-Pro.local>
2016-10-14 13:37:59 +0700

    fix margin in mobile

--- app/assets/javascripts/components/resend/wrapper.jsx
+++ app/assets/javascripts/components/resend/wrapper.jsx
@@ -19,16 +19,16 @@ class Wrapper extends React.Component {
       <div id="resend-wrapper" className="uk-width-1-1 full-height uk-flex uk-flex-middle">
         <div className="uk-width-8-10 uk-width-medium-6-10 uk-width-large-6-10 uk-container-center uk-flex uk-flex-center uk-flex-column">
           <Header />
-          <div className="uk-width-1-1 uk-text-center" style={{marginTop: "16vh"}}>
-            <h3>Missing the email? Try checking your spam and trash folder before requesting a resend</h3>
-            <h3>กรุณาตรวจสอบในโฟลเดอร์อีเมลขยะ หรือสแปมก่อนส่งอีเมลอีกครั้ง</h3>
+          <div className="uk-width-1-1 uk-text-center resend-text" style={{marginTop: "16vh"}}>
+            <h3 className="thin">Missing the email? Try checking your spam and trash folder before requesting a resend</h3>
+            <h3 className="thin">กรุณาตรวจสอบในโฟลเดอร์อีเมลขยะ หรือสแปมก่อนส่งอีเมลอีกครั้ง</h3>
           </div>
           <div className="uk-width-1-1 uk-flex uk-hidden-small">
             <div className="uk-width-8-10 email-input-wrapper">
-              <div>
-                <h2 className="uk-text-uppercase no-margin uk-margin-right">e-mail</h2>
+              <div className="uk-flex uk-flex-middle">
+                <h3 className="uk-text-uppercase no-margin uk-margin-right">e-mail</h3>
               </div>
-              <input ref="email" className="full-width-flex"/>
+              <input ref="email" className="full-width-flex" style={{paddingTop: 7, paddingBottom: 7}}/>
             </div>
             <div onClick={this.resend} className="uk-width-2-10 submit-button uk-flex uk-flex-middle pointer">
               <div className="uk-width-3-10 uk-container-center">
@@ -49,7 +49,7 @@ class Wrapper extends React.Component {
               </div>
             </div>
           </div>
-          <h3 className="uk-text-center" style={{display: this.state.email !== null ? "" : "none"}}>E-mail has been sent to {this.state.email}</h3>
+          <h3 className="uk-text-center thin" style={{display: this.state.email !== null ? "" : "none"}}>E-mail has been sent to {this.state.email}. Please check in the inbox.</h3>
         </div>
       </div>
     );
