d466610df9aecbb304a72c8d643867f1da21f4ce
Thongrapee <kulaspa@gmail.com>
2016-10-23 14:15:54 +0700

    Complete user update functionality

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,5 +8,26 @@ class Admin::UsersController < Admin::BaseController
   end
 
   def update
+    @user = User.friendly.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user.id), alert: 'User updated'
+    else
+      flash[:alert] = @user.errors.full_messages
+      render :show
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :email,
+      :first_name,
+      :last_name,
+      :shirt_size,
+      :twitter,
+      :workplace,
+      :website
+    )
   end
 end
