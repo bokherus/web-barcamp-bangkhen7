f35d804d0ee5265aae675fa286ad4544affd61af
TAWEESOFT <taweesoft@TAWEESOFTs-MacBook-Pro.local>
2016-11-11 08:36:47 +0700

    change reducer session's time and add afternoonbreak time

--- app/assets/javascripts/components/sessions/viewmodel.js
+++ app/assets/javascripts/components/sessions/viewmodel.js
@@ -1,13 +1,14 @@
 const moment = require('moment');
-let morningTime = [{start: '10:30', end: '11:00'}, {start: '11:00', end: '11:30'}, {start: '11:30', end: '12:00'} ];
-let afternoonTime = [{start: '13:00', end: '13:30'}, {start: '13:30', end: '14:00'}, {start: '14:00', end: '14:30'} ];
-
+let morningTime = [{start: '10:40', end: '11:05'}, {start: '11:10', end: '11:35'}, {start: '11:40', end: '12:05'} ];
+let afternoonTime = [{start: '13:00', end: '13:25'}, {start: '13:30', end: '13:55'}, {start: '14:00', end: '14:25'}, {start: '14:30', end: '14:55'} ];
+let afterBreakTime = [{start: '15:20', end: '15:45'}, {start: '15:50', end: '16:15'}, {start: '16:20', end: '16:45'}];
 let vm = (state, action) => {
   switch(action.type) {
     case 'init':
       state = {
-        morningSessions: dataFormater(morningTime, $.extend(true, {}, action.data)),
-        afternoonSessions: dataFormater(afternoonTime, $.extend(true, {}, action.data))
+        morningSessions: dataFormatter(morningTime, $.extend(true, {}, action.data)),
+        afternoonSessions: dataFormatter(afternoonTime, $.extend(true, {}, action.data)),
+        afterbreakSessions: dataFormatter(afterBreakTime, $.extend(true, {}, action.data))
       };
       return state;
     default:
@@ -15,7 +16,7 @@ let vm = (state, action) => {
   }
 };
 
-let dataFormater = (times,sessions) => {
+let dataFormatter = (times,sessions) => {
   let returnSessions = [];
   let validatedData = _.map(sessions, (session) => {
     let startTime      = moment(session.start_time).utcOffset('+0000').format('HH:mm');
@@ -33,8 +34,7 @@ let dataFormater = (times,sessions) => {
     });
     returnSessions.push(eachTime);
   });
+  console.log(returnSessions);
   return returnSessions;
 };
-
-vm.dataFormater = dataFormater;
 export default vm;
