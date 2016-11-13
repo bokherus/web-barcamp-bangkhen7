28f5b415829742e2d9d64995b597f6443b327f2a
TAWEESOFT <taweesoft@TAWEESOFTs-MacBook-Pro.local>
2016-11-14 06:20:57 +0700

    change cursor to pointer

--- app/assets/javascripts/components/walkin-registration/suggestion.jsx
+++ app/assets/javascripts/components/walkin-registration/suggestion.jsx
@@ -6,7 +6,7 @@ class Suggestion extends React.Component {
           {
             _.map(this.props.suggestion, (item, i) => {
               return (
-                <li key={i} className="list-group-item" onClick={this.props.onItemClick.bind(this,item)}>
+                <li key={i} className="list-group-item" onClick={this.props.onItemClick.bind(this,item)} style={{cursor: 'pointer'}}>
                   {
                     item.first_name
                   }
